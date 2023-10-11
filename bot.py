bb563c9104e382ed58fc89a54b6ee82dcb26aa0f
NebulaZer0 <blake.green@5CD2084X2G>
2023-10-11 09:30:10 -0500

    remove echo

--- bot.py
+++ bot.py
@@ -7,17 +7,6 @@ intents = discord.Intents.all()
 
 Bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
-#/echo hello
-@Bot.command()
-async def echo(ctx, *args) :
-    m_args = " ".join(args)
-    await ctx.send(m_args)
-#/Hello
-
-token = ""
-
-with open("token.txt") as file:
-    token = file.read()
 
 @Bot.command()
 async def pokemon(ctx):
